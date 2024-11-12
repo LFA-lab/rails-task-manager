3f03488678da17780aef215f82ab5822ab60cccb
Antoine LE FRAPPER <antoine.lefrapper@omexom.com>
2024-11-12 14:57:36 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,20 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Route pour afficher la liste des tâches
+  get 'tasks', to: 'tasks#index'
+
+  # Route pour afficher une tâche spécifique
+
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Route pour créer une nouvelle tâche
+  post 'tasks', to: 'tasks#create'
+
+  # Route pour éditer une tâche existante
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Route pour supprimer une tâche
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
